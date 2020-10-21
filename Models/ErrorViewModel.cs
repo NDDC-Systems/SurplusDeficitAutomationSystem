3335fd5c7545a6b78c7bd3bfa3af82f31fca7839
Dante Downie <ddlee1999@gmail.com>
2020-10-20 23:17:57 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SurplusDeficitAutomationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
