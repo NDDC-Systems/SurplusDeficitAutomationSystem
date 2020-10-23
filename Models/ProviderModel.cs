ef3f62e3ba8b2cfdfa0621a4bc4a4da77d94d1f6
Dante Downie <ddlee1999@gmail.com>
2020-10-23 03:47:20 -0400

    Added Models

---
+++ Models/ProviderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurplusDeficitAutomationSystem.Models
+{
+    public class ProviderModel
+    {
+        public int ProviderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Models/ReportModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurplusDeficitAutomationSystem.Models
+{
+    public class ReportModel
+    {
+        public int ContractId { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public string Provider { get; set; }
+        public Boolean Approved { get; set; }
+        public string Admin { get; set; }
+        public DateTime ApprovedDate { get; set; }
+    }
+}
