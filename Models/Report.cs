ebdb14ce1e0bbae0c76e56901f66412765abe1f5
Dante Downie <ddlee1999@gmail.com>
2020-12-04 10:52:51 -0500

    Updated model variables and removed in memory repository.

--- Models/Report.cs
+++ Models/Report.cs
@@ -8,11 +8,12 @@ namespace SurplusDeficitAutomationSystem.Models
     public class Report
     {
         public int ReportId { get; set; }
+        public int TemplateId { get; set; }
         public int ContractId { get; set; }
+        public int AdminId { get; set; }
         public DateTime SubmitDate { get; set; }
         public string Provider { get; set; }
         public Boolean Approved { get; set; }
-        public string Admin { get; set; }
         public DateTime? ApprovedDate { get; set; }
     }
 }
