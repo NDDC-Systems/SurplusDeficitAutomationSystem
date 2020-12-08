b9ed7b528cd9a4debf929d5a8ad1d28c64d40e29
Dante Downie <ddlee1999@gmail.com>
2020-12-08 18:51:06 -0500

    Made all interface repositories

---
+++ Models/IValuesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurplusDeficitAutomationSystem.Models
+{
+    public interface IValuesRepository
+    {
+        Values GetValue(int ValueId);
+        Values Add(Values value);
+        Values Update(Values valueChanges);
+        Values Delete(int ValueId);
+    }
+}
