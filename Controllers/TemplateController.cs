80eae639dfc8428de8eb0932d5a51ae80cdc39c6
Dante Downie <ddlee1999@gmail.com>
2020-12-08 23:47:40 -0500

    For viable product deliverable

--- Controllers/TemplateController.cs
+++ Controllers/TemplateController.cs
@@ -9,17 +9,30 @@ using System.Threading.Tasks;
 
 namespace SurplusDeficitAutomationSystem.Controllers
 {
+    [Route("Template")]
     public class TemplateController : Controller
     {
         private readonly ITemplateRepository _templateRepository;
+        //private readonly IFieldRepository _fieldRepository;
+        //private readonly ITemplateFieldsRepository _templateFieldsRepository;
         public TemplateController(ITemplateRepository templateRepository)
         {
             _templateRepository = templateRepository;
+            //_fieldRepository = fieldRepository;
+            //_templateFieldsRepository = templateFieldsRepository;
         }
+
+        [Route("")]
+        [Route("Index")]
         public ViewResult Index()
         {
-            var model = _templateRepository.GetAllTemplates();
-            return View(model);
+            ViewModel myModel = new ViewModel();
+            myModel.Templates = _templateRepository.GetAllTemplates();
+            //myModel.Fields = _fieldRepository.GetAllFields();
+            //myModel.TemplateFields = _templateFieldsRepository.GetAllTemplateFields();
+            return View(myModel);
+            //var model = _reportRepository.GetAllReports();
+            //return View(model);
         }
 
         [HttpGet]
@@ -38,5 +51,38 @@ namespace SurplusDeficitAutomationSystem.Controllers
 
             return View();
         }
+
+        /*[HttpGet]
+        public ViewResult NewField()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewField(Field field)
+        {
+            if (ModelState.IsValid)
+            {
+                Field newField = _fieldRepository.Add(field);
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ViewResult NewTemplateField()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewTemplateField (TemplateFields templateField)
+        {
+            if (ModelState.IsValid)
+            {
+                TemplateFields newTemplateField = _templateFieldsRepository.Add(templateField);
+            }
+
+            return View();
+        }*/
     }
 }
