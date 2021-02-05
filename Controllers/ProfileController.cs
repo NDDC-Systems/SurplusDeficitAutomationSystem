68991353a4beca2beb19576c19efd082e27dac6c
Noah Dancu <n01043349@unf.edu>
2021-02-04 19:48:21 -0500

    account

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -16,5 +16,18 @@ namespace SurplusDeficitAutomationSystem.Controllers
         {
             return View();
         }
+        public ActionResult Accounts()
+        {
+            //var allusers = context.Users.ToList();
+            //var users = allusers.Where(x => x.Roles.Select(role => role.Name).Contains("User")).ToList();
+            //var userVM = users.Select(user => new UserViewModel { Username = user.FullName, Roles = string.Join(",", user.Roles.Select(role => role.Name)) }).ToList();
+
+            // var admins = allusers.Where(x => x.Roles.Select(role => role.Name).Contains("Admin")).ToList();
+            //var adminsVM = admins.Select(user => new UserViewModel { Username = user.FullName, Roles = string.Join(",", user.Roles.Select(role => role.Name)) }).ToList();
+            //var model = new GroupedUserViewModel { Users = userVM, Admins = adminsVM };
+
+            //return View(model);
+            return View();
+        }
     }
 }
