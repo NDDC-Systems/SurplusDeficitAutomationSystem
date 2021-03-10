384fb09762f66f46e9f43b51375b74436c675c93
Noah Dancu <n01043349@unf.edu>
2021-03-10 18:08:33 -0500

    roles

---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurplusDeficitAutomationSystem.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role")]
+        public string RoleName { get; set; }
+    }
+}
